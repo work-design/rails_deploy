8fd60e2ecadade7c16f3e7543d5d06b3fd313b4f
qinmingyuan <mingyuan0715@foxmail.com>
2024-09-30 10:46:15 +0800

    update kmal logic

--- lib/kamal_override/configuration.rb
+++ lib/kamal_override/configuration.rb
@@ -2,7 +2,7 @@ module KamalOverride
   module Configuration
 
     def proxy_image
-      'ccr.ccs.tencentyun.com/work-design/kamal-proxy:v0.4.0'
+      'ccr.ccs.tencentyun.com/kamal/kamal-proxy:latest'
     end
 
   end
