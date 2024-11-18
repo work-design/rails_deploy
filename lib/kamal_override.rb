78a120a06edde9def946cba043ec3d4715c5baa0
qinmingyuan <mingyuan0715@foxmail.com>
2024-11-18 12:12:31 +0800

    postion

--- lib/kamal_override.rb
+++ lib/kamal_override.rb
@@ -1,9 +1,9 @@
+require 'kamal_override/build_clone'
+require 'kamal_override/configuration'
+
 require 'kamal_override/commands/clone'
 require 'kamal_override/commands/docker'
 require 'kamal_override/commands/proxy'
 
 require 'kamal_override/configuration/proxy'
 require 'kamal_override/configuration/role'
-
-require 'kamal_override/build_clone'
-require 'kamal_override/configuration'
