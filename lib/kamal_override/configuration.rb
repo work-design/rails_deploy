08fd467687028ba5ac0b76f8cb0b6a01a34f06ba
qinmingyuan <mingyuan0715@foxmail.com>
2025-06-05 12:20:42 +0800

    update

--- lib/kamal_override/configuration.rb
+++ lib/kamal_override/configuration.rb
@@ -2,7 +2,7 @@ module KamalOverride
   module Configuration
 
     def proxy_image
-      'ccr.ccs.tencentyun.com/kamal/kamal-proxy:v0.8.7'
+      'ccr.ccs.tencentyun.com/kamal/kamal-proxy:v0.9.1'
     end
 
   end
