6a640e579537b4a3c7f40f489f497c8c373ee9f5
qinmingyuan <mingyuan0715@foxmail.com>
2025-06-05 12:37:10 +0800

    moved unused cods

--- lib/kamal_override.rb
+++ lib/kamal_override.rb
@@ -1,9 +1,7 @@
 require 'kamal_override/build_clone'
-require 'kamal_override/configuration'
 
 require 'kamal_override/commands/clone'
 require 'kamal_override/commands/docker'
-require 'kamal_override/commands/proxy'
 
 require 'kamal_override/configuration/proxy'
 require 'kamal_override/configuration/role'
